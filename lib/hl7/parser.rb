0f23a9403655e75c225e4a8b1eb73d2ee3b724d3
Jordan Ryan Reuter <jordan.reuter@heartit.com>
2016-04-27 16:44:06 -0400

    Parse entire messages

--- lib/hl7/parser.rb
+++ lib/hl7/parser.rb
@@ -4,40 +4,56 @@ module HL7
   # TODO: doc
   # :nodoc:
   class Parser < Parslet::Parser
-    rule(:sub_component) { match['^^&|\r'].repeat(1).as(:sub_component) }
-    rule(:sub_components) do
+    rule(:segment_delimiter) { str("\r") }
+    rule(:field_delimiter) { str('|') }
+    rule(:component_delimiter) { str('^') }
+    rule(:sub_component_delimiter) { str('&') }
+    rule(:escape) { str('\\') }
+    rule(:repetition_delimiter) { str('~') }
+    rule(:normal_character) do
       (
-        sub_component >>
-        str('&').maybe
-      ).repeat(1)
+        segment_delimiter |
+        field_delimiter |
+        component_delimiter |
+        sub_component_delimiter |
+        escape |
+        repetition_delimiter
+      ).absent? >> any
+    end
+    rule(:sub_component) { normal_character.repeat(1).as(:sub_component) }
+    rule(:sub_components) do
+      (sub_component >> sub_component_delimiter.maybe).repeat(1)
     end
     rule(:component) { sub_components.repeat(1).as(:component) }
-    rule(:components) do
-      (
-        component >>
-        str('^').maybe
-      ).repeat(1)
-    end
+    rule(:components) { (component >> component_delimiter.maybe).repeat(1) }
     rule(:field) { components.repeat(1).as(:field) }
-    rule(:fields) do
-      (
-        field >>
-        str('|').maybe
-      ).repeat(1)
-    end
+    rule(:fields) { (field >> field_delimiter.maybe).repeat(1) }
+    rule(:segment_type) { match['[:alnum:]'].repeat(3, 3).as(:type) }
     rule(:segment) do
       (
-        match['[:alnum:]'].repeat(3, 3).as(:type) >>
-        str('|') >>
-        fields.repeat(1).as(:fields)
+        segment_type >>
+        field_delimiter >>
+        fields.repeat.as(:fields)
       ).as(:segment)
     end
     rule(:msh_segment) do
       (
         str('MSH').as(:type) >>
-        str('|^~\\&|') >>
-        fields.repeat(1).as(:fields)
-      ).as(:segment)
+        field_delimiter >>
+        component_delimiter >>
+        repetition_delimiter >>
+        escape >>
+        sub_component_delimiter >>
+        field_delimiter >>
+        fields.repeat.as(:fields)
+      ).as(:header)
+    end
+    rule(:message) do
+      (
+        msh_segment >>
+        segment_delimiter >>
+        segment.repeat.as(:segments)
+      ).as(:message)
     end
   end
 end
--- test/hl7/parser_test.rb
+++ test/hl7/parser_test.rb
@@ -112,7 +112,7 @@ class HL7ParserTest < Minitest::Test
       parse_method: :msh_segment,
       input: 'MSH|^~\\&|another^field',
       expectation: {
-        segment: {
+        header: {
           type: 'MSH',
           fields: [
             { field: [
@@ -122,75 +122,26 @@ class HL7ParserTest < Minitest::Test
           ]
         }
       }
+    },
+    {
+      parse_method: :message,
+      input: "MSH|^~\\&|\rPID|foo|bar",
+      expectation: {
+        message: {
+          header: { type: 'MSH', fields: [] },
+          segments: [
+            { segment: {
+              type: 'PID',
+              fields: [
+                { field: [{ component: [{ sub_component: 'foo' }] }] },
+                { field: [{ component: [{ sub_component: 'bar' }] }] }
+              ] }
+            }
+          ]
+        }
+      }
     }
   ].each do |h|
     equality_test_for(h)
-  end
-
-  def test_msh_is_first_segment
-    skip
-    good = 'MSH'
-    bad = 'ZZZ'
-    @klass.new good
-    assert_raises(ArgumentError) { @klass.new bad }
-  end
-
-  def test_delimiter_detection
-    skip
-    defaults = 'MSH|^~\\&'
-    d = @klass.new(defaults).delimiters
-    assert_equal '|', d.field
-    assert_equal '^', d.component
-    assert_equal '~', d.repetition
-    assert_equal '\\', d.escape
-    assert_equal '&', d.sub_component
-  end
-
-  def test_unconventional_delimiter_detection
-    skip
-    message = 'MSHhjkl;'
-    d = @klass.new(message).delimiters
-    assert_equal 'h', d.field
-    assert_equal 'j', d.component
-    assert_equal 'k', d.repetition
-    assert_equal 'l', d.escape
-    assert_equal ';', d.sub_component
-  end
-
-  # rubocop:disable MethodLength
-  def test_accession
-    skip
-    message = <<HL7.freeze
-MSH|^~\\&|RADIS1||DMCRES||19940502161633||ORU|19940502161633|D|2.2|964||AL|AL
-PID|||N00803||RADIOLOGY^INPATIENT^SIX||19520606|F||A||||||||003555
-PV1||I|N77^7714^01|||||||OB
-OBR|1|003555.0015.001^DMCRES|0000000566^RADIS1|37953^CT CHEST^L|||199405021545|||||||||||||0000763||||CT|P||||||R/O TUMOR|202300^BAKER^MARK^E|||01^LOCHLEAR, JUDY
-OBX||TX|FIND^FINDINGS^L|1|This is a test on 05/02/94.
-OBX||TX|FIND^FINDINGS^L|2|This is a test for the CRR.
-OBX||TX|FIND^FINDINGS^L|3|This is a test result to generate multiple obr's to check the cost
-OBX||TX|FIND^FINDINGS^L|4|display for multiple exams.
-OBX||TX|FIND^FINDINGS^L|5|APPROVING MD:
-OBR|2|^DMCRES|0000000567^RADIS1|37956^CT ABDOMEN^L|||199405021550|||||||||||||0000763|||||P||||||R/O TUMOR|202300^BAKER^MARK^E|||01^LOCHLEAR, JUDY
-OBR|3|^DMCRES|0000000568^RADIS1|37881^CT PELVIS (LIMITED)^L|||199405021551|||||||||||||0000763|||||P||||||R/O TUMOR|202300^BAKER^MARK^E|||01^LOCHLEAR, JUDY
-HL7
-    parsed = @klass.new(message).parse
-    assert_equal '|', parsed
-      .segments[0]
-      .fields[0]
-    assert_equal '^~\\&', parsed
-      .segments[0]
-      .fields[1]
-    assert_equal '7714', parsed
-      .segments[2]
-      .fields[2]
-      .components[0][1]
-      .sub_components[0]
-      .content
-    assert_equal '19940502161633', parsed
-      .segments[0]
-      .fields[6]
-      .components[0][0]
-      .sub_components[0]
-      .content
   end
 end
